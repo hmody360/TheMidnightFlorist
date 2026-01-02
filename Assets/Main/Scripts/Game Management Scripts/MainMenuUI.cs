a87a0bf82026a9b0d1fdb8fcc5403e4d172b7d52
Mohammed Soqati <hmadahmody360@gmail.com>
2026-01-02 20:44:55 +0300

    Added Dynamic Camera Movement and World Space UI

--- Assets/Main/Scripts/Game Management Scripts/MainMenuUI.cs
+++ Assets/Main/Scripts/Game Management Scripts/MainMenuUI.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,6 +9,7 @@ public class MainMenuUI : MonoBehaviour
     [SerializeField] private GameObject _settingsHolder;
     [SerializeField] private GameObject _howToPlayDayHolder;
     [SerializeField] private GameObject _howToPlayNightHolder;
+    [SerializeField] private CinemachineCamera[] _mainMenuCameras;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,31 +21,35 @@ public class MainMenuUI : MonoBehaviour
     private void ShowMainMenu()
     {
         _mainMenuHolder.SetActive(true);
+        ChangeCameraBasedOnIndex(0);
     }
 
     private void HideMainMenu()
     {
-        _mainMenuHolder.SetActive(false);
+        _mainMenuHolder.GetComponent<ImageUIFader>().Hide();
+
     }
 
     private void ShowSettings()
     {
         _settingsHolder.SetActive(true);
+        ChangeCameraBasedOnIndex(1);
     }
 
     private void HideSettings()
     {
-        _settingsHolder.SetActive(false);
+        _settingsHolder.GetComponent<ImageUIFader>().Hide();
     }
 
     private void ShowHowToPlayDay()
     {
         _howToPlayDayHolder.SetActive(true);
+        
     }
 
     private void HideHowToPlayDay()
     {
-        _howToPlayDayHolder.SetActive(false);
+        _howToPlayDayHolder.GetComponent<ImageUIFader>().Hide();
     }
 
     private void ShowHowToPlayNight()
@@ -52,7 +59,7 @@ public class MainMenuUI : MonoBehaviour
 
     private void HideHowToPlayNight()
     {
-        _howToPlayNightHolder.SetActive(false);
+        _howToPlayNightHolder.GetComponent<ImageUIFader>().Hide();
     }
 
     public void OpenSettingsPanel()
@@ -71,30 +78,35 @@ public class MainMenuUI : MonoBehaviour
     {
         HideMainMenu();
         ShowHowToPlayDay();
+        StartCoroutine(GoThroughOtherCamera(1, 2));
     }
 
     public void CloseHowToPlayDay()
     {
         ShowMainMenu();
         HideHowToPlayDay();
+        StartCoroutine(GoThroughOtherCamera(1, 0));
     }
 
     public void OpenHowToPlayNight()
     {
         HideHowToPlayDay();
         ShowHowToPlayNight();
+        ChangeCameraBasedOnIndex(3);
     }
 
     public void CloseHowToPlayNight()
     {
         ShowMainMenu();
         HideHowToPlayNight();
+        StartCoroutine(GoThroughOtherCamera(1, 0));
     }
 
     public void GoBackToHowToPlayDay()
     {
         HideHowToPlayNight();
         ShowHowToPlayDay();
+        ChangeCameraBasedOnIndex(2);
     }
 
     public void StartGame()
@@ -105,6 +117,35 @@ public class MainMenuUI : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    // Camera Transitions
+
+    private void ChangeCameraBasedOnIndex(int index)
+    {
+        if (index >= _mainMenuCameras.Length)
+        {
+            Debug.LogError("Camera Index is not in the Camera List");
+            return;
+        }
+        for (int i = 0; i < _mainMenuCameras.Length; i++)
+        {
+            if (i == index)
+            {
+                _mainMenuCameras[i].Priority = 1;
+            }
+            else
+            {
+                _mainMenuCameras[i].Priority = 0;
+            }
+        }
+    }
+
+    private IEnumerator GoThroughOtherCamera(int throughCameraIndex, int finalCameraIndex)
+    {
+        ChangeCameraBasedOnIndex(throughCameraIndex);
+        yield return new WaitForSeconds(1.3f);
+        ChangeCameraBasedOnIndex(finalCameraIndex);
     }
 
 }
